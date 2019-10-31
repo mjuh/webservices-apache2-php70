a135ba3b9faf16b6d8ce768f78aeeae804316f29
Oleg Pykhalov <go.wigust@gmail.com>
2019-10-31 14:19:08 +0300

    default.nix: Add cacert

--- default.nix
+++ default.nix
@@ -45,7 +45,7 @@ pkgs.dockerTools.buildLayeredImage rec {
     coreutils
     libjpeg_turbo
     (optipng.override{ inherit libpng ;})
-    gifsicle
+    gifsicle cacert
   ]
   ++ collect isDerivation mjperl5Packages
   ++ collect isDerivation phpPackages.php70Packages;
